4e5430959392a9f6a4c2b27ca5e38176e2ff5ff3
Lee816 <lgha0816@naver.com>
2023-07-17 10:14:53 +0900

    프로젝트 생성 위치 변경

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9v0*^9^fhi5bb!c@3ea52qv)=hwhi!7i&#zzowab*=qnl#c^+b"
+SECRET_KEY = "django-insecure-#*-%ath6h*45rnu&ce+x69sfn34@2#-m7s)bc^*21%x&u1%vd^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
