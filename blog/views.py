af2883f013544715cfab8652c93e9a909bba93c3
Lee816 <lgha0816@naver.com>
2023-07-19 11:26:08 +0900

    페이지네이션 오류 페이지 연결

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
@@ -12,7 +12,14 @@ def post_list(request):
     # 페이지당 3개의 게시물로 페이지네이션
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger or TypeError:
+        # page_number가 정수가 아닌 경우 첫 번째 페이지 제공
+        posts = paginator.page(1)
+    except EmptyPage:
+        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
