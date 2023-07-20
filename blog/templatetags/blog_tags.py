fe7079cc802a1c6242c8b7994e177c07d0851e9b
Lee816 <lgha0816@naver.com>
2023-07-20 14:15:42 +0900

    markdown 구문을 지원하는 템플릿 필터 생성

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+
+import markdown
 
 from ..models import Post
 
@@ -26,3 +29,10 @@ def get_most_commented_posts(count=5):
     )[:count]
     # annotate() 함수를 사용하여 각 포스트에 대한 총 댓글 수를 집계
     # Count 집계함수를 이용해 각 Post 객체에 계산된 total_comments 필드에 댓글 수를 저장
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
+    # mark_safe 함수를 사용하여 결과를 HTML로 표시 ( Django는 어떤 HTML 코드도 신뢰하지 않고 출력하기전에 이스케이프 처리하기 때문)
