f5dd7e4641439cb17d5ecf05b17a4d0deecd94ff
Lee816 <lgha0816@naver.com>
2023-07-20 10:50:18 +0900

    태그 url 연결

--- blog/urls.py
+++ blog/urls.py
@@ -3,17 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
-# urlpatterns = [
-#     path("", views.post_list, name="post_list"),
-#     path(
-#         "<int:year>/<int:month>/<int:day>/<str:slug>/",
-#         views.post_detail,
-#         name="post_detail",
-#     ),
-# ]
-
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
+    path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
     path(
         "<int:year>/<int:month>/<int:day>/<str:slug>/",
         views.post_detail,
