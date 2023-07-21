f4817bfd10fa2beee015f4b3627ccb59b18c142e
Lee816 <lgha0816@naver.com>
2023-07-21 14:05:47 +0900

    사용자 정의 검색 뷰 로직 추가

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,11 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count  # Django ORM의 Count 집계 함수
 from taggit.models import Tag
-
-from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.postgres.search import SearchVector
 
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 # Create your views here.
 
@@ -119,6 +118,25 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
 
 
