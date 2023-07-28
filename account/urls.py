04d9df8f6513705edd4b56606be4ee52e6f3d84c
Lee816 <lgha0816@naver.com>
2023-07-28 13:41:20 +0900

    LogoutView 가 get 요청이 막혀서 auth 클래스뷰를 사용하지 않고 직접 구현

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 app_name = "account"
 urlpatterns = [
-    # path("login/", views.user_login, name="login"),
+    # path("login/", views.user_login2, name="login"),
     path("", views.dashboard, name="dashboard"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
@@ -8,7 +8,7 @@ from .forms import LoginForm
 # Create your views here.
 
 
-def user_login(request):
+def user_login2(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -28,7 +28,35 @@ def user_login(request):
                 return HttpResponse("Invalid login")
     else:
         form = LoginForm()
+    return render(request, "account/login2.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )  # authenticate() 메서드를 사용해 데이터베이스에 대해 인증 성공적으로 인증하면 User 객체를 반환, 그렇지 않으면 None을 반환
+            if user is not None:
+                if user.is_active:  # Django의 User 모델의 속성인 is_active 속성의 상태를 확인
+                    login(
+                        request, user
+                    )  # authenticate()는 사용자 자격증명, login()은 현재 세션에 사용자를 설정
+                    return redirect("account:dashboard")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("account:login")
 
 
 @login_required  # 현재 사용자가 인증된 사용자인지 확인하는 데코레이터
