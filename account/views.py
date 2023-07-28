5c6ebabd0f8e37d54d38ef72b25ce36567a2f629
Lee816 <lgha0816@naver.com>
2023-07-28 11:43:26 +0900

    account 앱 로그인상태를 요구하는 대시보드 뷰 추가

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -28,3 +29,8 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+@login_required  # 현재 사용자가 인증된 사용자인지 확인하는 데코레이터
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
