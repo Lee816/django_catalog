cc3c271cb112a6a943c354108e0b69f5551592c7
Lee816 <lgha0816@naver.com>
2023-07-28 11:19:45 +0900

    account 앱 urls.py 오타 수정

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ app_name = "account"
 urlpatterns = [
     # path("login/", views.user_login, name="login"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("login/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
