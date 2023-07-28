348974ebad149f0a9334bd94e7db760ca8d1384c
Lee816 <lgha0816@naver.com>
2023-07-28 15:19:47 +0900

    account 앱 회원가입 폼 생성

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())  # 패스워드타입 input을 사용하기위해
+
+
+class UserRegistrationsForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["username", "fitst_name", "email"]
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:  # 두개의 비밀번호를 비교하고 다르면 에러 발생
+            raise forms.ValidationError("Passwors don't match.")
+        return cd["password2"]
