38458914c55537dc2e158d9c552294f6bf83acd3
Lee816 <lgha0816@naver.com>
2023-07-31 10:17:58 +0900

    유저 정보 편집 폼 생성

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -20,3 +22,15 @@ class UserRegistrationsForm(forms.ModelForm):
         if cd["password"] != cd["password2"]:  # 두개의 비밀번호를 비교하고 다르면 에러 발생
             raise forms.ValidationError("Passwors don't match.")
         return cd["password2"]
+
+
+class UserEditForm(forms.ModelForm):  # 사용자의 이름과 이메일을 편집하는 폼
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+
+class ProfileEditForm(forms.ModelForm):  # 사용자의 확장모델의 생일과 프로필사진을 편집하는 폼
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
