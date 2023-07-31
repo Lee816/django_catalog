d6ef0c82e3e7eba2b949141dca583aca5a98fb6a
Lee816 <lgha0816@naver.com>
2023-07-31 10:25:36 +0900

    raw_id_fields 설명 주석

--- account/admin.py
+++ account/admin.py
@@ -8,4 +8,6 @@ from .models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "date_of_birth", "photo"]
-    raw_id_fields = ["user"]
+    raw_id_fields = [
+        "user"
+    ]  # 데이터의 양이 많을 경우 드롭다운 방식은 어렵기 때문에 raw_id_fields 를 사용하면 input으로 변경됨
