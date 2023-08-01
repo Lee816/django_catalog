163e205a8df9a0b3582d80dbdc255d1b9ec44455
Lee816 <lgha0816@naver.com>
2023-08-01 10:07:11 +0900

    ALLOWED_HOSTS 도메인 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secrets["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 SITE_ID = 1
