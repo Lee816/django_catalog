46c3db0f6a6355df259945c06ac980dc136126c5
Lee816 <lgha0816@naver.com>
2023-08-03 13:07:54 +0900

    팔로우 시스템 모델 생성

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -11,3 +12,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+class Contact(models.Model):
+    # 관계를 생성하는 사용자에 대한 외래키
+    user_from = models.ForeignKey('auth.User',related_name='rel_from_set',on_delete=models.CASCADE)
+    # 팔로우 되는 사용자에 대한 외래키
+    user_to = models.ForeignKey('auth.User',related_name='rel_to_set',on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+    
+user_model = get_user_model()
+user_model.add_to_class('following',models.ManyToManyField('self',through=Contact,related_name='followers',symmetrical=False))
