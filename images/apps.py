c5b7d5716a54fc8f7a68125f7e60196abe6db342
Lee816 <lgha0816@naver.com>
2023-08-04 13:16:02 +0900

    images 앱이 로드될때 이 앱의 신호가 임포트 되도록 한다.

--- images/apps.py
+++ images/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "images"
+    
+    # 이 앱의 신호를 임포트 하여, 신호가 images 앱이 로드될때 임포트 되도록 한다.
+    def ready(self):
+        # import signal handlers
+        import images.signals
