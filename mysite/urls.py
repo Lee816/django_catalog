0f266a3340af7bc74f236c495bc11f06af8aba78
Lee816 <lgha0816@naver.com>
2023-08-08 13:36:44 +0900

    orders 앱 url 연결

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('shop/',include('shop.urls',namespace='shop')),
     path('cart/',include('cart.urls',namespace='cart')),
-    
+    path('orders/',include('orders.urls',namespace='orders')),
 ]
 
 if settings.DEBUG:
