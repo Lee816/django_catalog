a4331a9a217d1723257725537547218a5099d715
Lee816 <lgha0816@naver.com>
2023-08-14 11:09:07 +0900

    order 앱 주문 생성후 결제페이지 url 연결

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import OrderItem
 from .forms import OrderCreateForm
@@ -21,8 +22,12 @@ def order_create(request):
             cart.clear()
             # 비동기 실행
             order_created.delay(order.id)
+            # 세션에 주문정보 등록
+            request.session['order_id'] = order.id
+            # 결제시스템에 리다이렉트
+            return redirect(reverse('payment:process'))
             
-            return render(request,'orders/order/created.html', {'order':order})
+            # return render(request,'orders/order/created.html', {'order':order})
 
     else:
         form = OrderCreateForm()
