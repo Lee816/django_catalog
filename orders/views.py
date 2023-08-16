d6903ef708261de0e1470725689e0a74d042ff93
Lee816 <lgha0816@naver.com>
2023-08-16 14:23:18 +0900

    pdf 전환 뷰와 템플릿 작성

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
@@ -41,3 +45,17 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request,'admin/orders/order/detail.html', {'order':order})
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    # 장고에서 제공하는 render_to_string()함수를 사용하여 orders/order/pdf.html을 렌더링
+    html = render_to_string('orders/order/pdf.html',{'order':order})
+    # application/pdf 콘텐츠 유형을 지정
+    # 파일이름을 지정하기 위해 Content-Disposition 헤더를 포함하는 새 HttpResponse 객체를 생성
+    response = HttpResponse(content_type='applecaion/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    # WeasyPrint 를 사용하여 렌더링된 HTML 코드에서 PDF 파일을 생성하고 HttpResponse 객체인 response에 파일을 쓰기
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT/'css/pdf.css')])
+
+    return response
