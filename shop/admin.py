c27b75af85f2ac0a3228c68931bc0778bf5fa667
Lee816 <lgha0816@naver.com>
2023-08-22 14:25:23 +0900

    번역을 관리자 사이트에 통합

--- shop/admin.py
+++ shop/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
 
 from .models import Category, Product
 
 # Register your models here.
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ['name','slug']
+    
     # prepopulated_fields 는 slug 필드에 name필드의 내용을 자동으로 입력해 준다.
-    prepopulated_fields = {'slug': ('name',)}
+    def get_prepopulated_fields(self,request, obj=None):
+        return {'slug': ('name',)}
     
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ['name','slug','price','available','created','updated']
     list_filter = ['available','created','updated']
     # 목록 표시 페이지에서 편집 가능한 필드를 설정하며 list_display에도 같이 등록 되어 있어야 한다.
     list_editable = ['price','available']
-    prepopulated_fields = {'slug': ('name',)}
+    
+    def get_prepopulated_fields(self,request, obj=None):
+        return {'slug': ('name',)}
